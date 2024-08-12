52059ac1ca270c20e75734a3bf3f5532ac24b115
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-08-12 11:06:31 +0900

    :tada: init: init project

---
+++ src/main/java/com/nemo/oceanAcademy/OceanAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.oceanAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemo/oceanAcademy/OceanAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.oceanAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
