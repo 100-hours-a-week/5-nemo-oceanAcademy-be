2b7428ae8eef61cca26e7a44f72d6edb9473d5b8
sodaakim <sohuikim2020@naver.com>
2024-08-19 17:29:33 +0900

    :sparkles: update 충돌제거

--- src/main/java/com/nemo/oceanAcademy/domain/user/controller/UserController.java
+++ src/main/java/com/nemo/oceanAcademy/domain/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.nemo.oceanAcademy.domain.user.controller;
 
-import com.nemo.oceanAcademy.domain.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,24 +8,4 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    // 닉네임 중복 검사
-    @GetMapping("/checkNickname")
-    public ResponseEntity<Boolean> checkNickname(@RequestParam String nickname) {
-        boolean exists = userService.isNicknameTaken(nickname);
-        return ResponseEntity.ok(exists);
-    }
-
-    // 회원탈퇴 (소프트 삭제)
-    @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteUser() {
-        userService.deleteCurrentUser();
-        return ResponseEntity.noContent().build();
-    }
 }
--- src/main/java/com/nemo/oceanAcademy/domain/user/service/UserService.java
+++ src/main/java/com/nemo/oceanAcademy/domain/user/service/UserService.java
@@ -1,47 +1,7 @@
-import com.nemo.oceanAcademy.domain.user.entity.User;
-import com.nemo.oceanAcademy.domain.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.nemo.oceanAcademy.domain.user.service;
 
 import java.util.Optional;
 
-@Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    // JWT 토큰에서 추출된 사용자 ID로 유저 정보를 조회하는 메서드
-    public Optional<User> getUserById(String userId) {
-        return userRepository.findById(userId);
-    }
-
-    // 새로운 유저 생성 (OAuth를 통해 받은 유저 정보 저장)
-    public User createUser(String userId, String nickname, String profileImagePath) {
-        User newUser = User.builder()
-                .id(userId)
-                .nickname(nickname)
-                .profileImagePath(profileImagePath)
-                .build();
-        return userRepository.save(newUser);
-    }
-
-    // 유저 정보 수정 (프로필, 닉네임 등)
-    public User updateUser(String userId, String newNickname, String newProfileImagePath) {
-        Optional<User> user = userRepository.findById(userId);
-        if (user.isPresent()) {
-            User updatedUser = user.get();
-            updatedUser.setNickname(newNickname);
-            updatedUser.setProfileImagePath(newProfileImagePath);
-            return userRepository.save(updatedUser);
-        }
-        return null;
-    }
-
-    // 유저 탈퇴 (soft delete)
-    public void deleteUser(String userId) {
-        Optional<User> user = userRepository.findById(userId);
-        user.ifPresent(User::softDelete);
-        userRepository.save(user.get());
-    }
 }
