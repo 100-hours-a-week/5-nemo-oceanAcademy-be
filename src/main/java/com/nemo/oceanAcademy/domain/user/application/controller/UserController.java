8f06759e2cd77ec118c60fc3d6cb0178fa7c26b8
sodaakim <sohuikim2020@naver.com>
2024-08-30 19:58:20 +0900

    :sparkles: update classroom 센트리

--- src/main/java/com/nemo/oceanAcademy/domain/user/application/controller/UserController.java
+++ src/main/java/com/nemo/oceanAcademy/domain/user/application/controller/UserController.java
@@ -8,7 +8,6 @@ import com.nemo.oceanAcademy.domain.user.application.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import jakarta.servlet.http.HttpServletRequest;
