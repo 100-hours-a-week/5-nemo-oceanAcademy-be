b832db29ffa35bc1d1bf0eb915ab450177c30eb4
sodaakim <sohuikim2020@naver.com>
2024-09-26 13:38:11 +0900

    :sparkles: DB 도메인 설정 변경

--- src/main/java/com/nemo/oceanAcademy/common/db/SubdomainRoutingDataSource.java
+++ src/main/java/com/nemo/oceanAcademy/common/db/SubdomainRoutingDataSource.java
@@ -13,29 +13,36 @@ public class SubdomainRoutingDataSource extends AbstractRoutingDataSource {
         // RequestContextHolder를 통해 현재 HTTP 요청 컨텍스트가 있는지 확인
         if (RequestContextHolder.getRequestAttributes() instanceof ServletRequestAttributes) {
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            String host = request.getServerName();  // 서버 네임을 감지
 
-            if (host == null || host.isEmpty()) {
-                throw new IllegalStateException("서버 호스트 정보를 찾을 수 없습니다.");
-            }
+            // Referer와 Origin 헤더를 가져옴
+            String referer = request.getHeader("Referer");
+            String origin = request.getHeader("Origin");
 
-            System.out.println("도메인: " + host);
-
-            // dev 서브 도메인이거나 로컬 환경이면 dev 데이터베이스 사용
-            if (host.startsWith("dev.") || "localhost".equals(host)) {
+            // referer나 origin이 localhost일 때 dev 데이터베이스 사용
+            if ((referer != null && referer.contains("localhost")) || (origin != null && origin.contains("localhost"))) {
+                System.out.println("로컬 환경이 감지되었습니다. dev 데이터베이스를 사용합니다.");
                 return "dev";
             }
 
-            // www.nemooceanacademy.com일 때 prod 데이터베이스 사용
-            if ("www.nemooceanacademy.com".equals(host)) {
+            // referer나 origin이 dev.nemooceanacademy.com일 때 dev 데이터베이스 사용
+            if ((referer != null && referer.contains("dev.nemooceanacademy.com")) ||
+                    (origin != null && origin.contains("dev.nemooceanacademy.com"))) {
+                System.out.println("개발 환경이 감지되었습니다. dev 데이터베이스를 사용합니다.");
+                return "dev";
+            }
+
+            // referer나 origin이 www.nemooceanacademy.com일 때 prod 데이터베이스 사용
+            if ((referer != null && referer.contains("www.nemooceanacademy.com")) ||
+                    (origin != null && origin.contains("www.nemooceanacademy.com"))) {
+                System.out.println("프로덕션 환경이 감지되었습니다. prod 데이터베이스를 사용합니다.");
                 return "prod";
             }
 
-            // 호스트가 예상 범위 내에 없는 경우 기본 prod로 설정
-            return null;
+            // 호스트가 예상 범위 내에 없는 경우 기본 데이터베이스 사용
+            return null;  // 기본 데이터베이스 사용
         }
 
-        // HTTP 요청 컨텍스트가 없으면 기본 데이터베이스 사용 (기본 datasource 아래 설정 사용)
-        return null;
+        // HTTP 요청 컨텍스트가 없으면 기본 데이터베이스 사용
+        return null;  // 기본 데이터베이스 사용
     }
 }
