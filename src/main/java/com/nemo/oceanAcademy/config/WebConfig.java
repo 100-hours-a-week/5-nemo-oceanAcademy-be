850f5b06e8d2cbdde69f755dae518bcf6d043936
sodaakim <sohuikim2020@naver.com>
2024-09-26 12:46:35 +0900

    :sparkles: CORS 수정

--- src/main/java/com/nemo/oceanAcademy/config/WebConfig.java
+++ src/main/java/com/nemo/oceanAcademy/config/WebConfig.java
@@ -1,37 +1,29 @@
 package com.nemo.oceanAcademy.config;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://43.201.137.157:3000",
-                "https://43.201.137.157:3000",
-                "http://nemooceanacademy.com",
-                "https://nemooceanacademy.com",
-                "http://www.nemooceanacademy.com",
-                "https://www.nemooceanacademy.com",
-                "http://dev.nemooceanacademy.com",
-                "https://dev.nemooceanacademy.com"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+                // Frontend
+                .allowedOrigins("http://localhost:3000",
+                                "http://43.201.137.157:3000",
+                                "https://43.201.137.157:3000",
+                                "http://nemooceanacademy.com",
+                                "https://nemooceanacademy.com",
+                                "http://www.nemooceanacademy.com",
+                                "https://www.nemooceanacademy.com",
+                                "http://dev.nemooceanacademy.com",
+                                "https://dev.nemooceanacademy.com"
+                )
+
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
